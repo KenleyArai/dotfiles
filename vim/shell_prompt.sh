0479fe2cc79df25ba26db8b3de02016f092a1cfc
Kenley Arai <karai001@ucr.edu>
2014-12-01 10:05:50 -0800

    Fixed up tmuxline and powerline

--- vim/shell_prompt.sh
+++ vim/shell_prompt.sh
@@ -21,13 +21,7 @@ function __promptline_ps1 {
   slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "b" slices
-  __promptline_wrapper "$USER" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "c" header
-  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_wrapper "$(if [[ -n ${ZSH_VERSION-} ]]; then print %n; elif [[ -n ${FISH_VERSION-} ]]; then printf "%s" "$USER"; else printf "%s" \\u; fi )" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
 
   # section "y" header
   slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_sep}${space}" slice_empty_prefix="${y_fg}${y_bg}${space}"
@@ -58,36 +52,6 @@ function __promptline_vcs_branch {
   fi
   return 1
 }
-function __promptline_cwd {
-  local dir_limit="3"
-  local truncation="⋯"
-  local first_char
-  local part_count=0
-  local formatted_cwd=""
-  local dir_sep="  "
-  local tilde="~"
-
-  local cwd="${PWD/#$HOME/$tilde}"
-
-  # get first char of the path, i.e. tilde or slash
-  [[ -n ${ZSH_VERSION-} ]] && first_char=$cwd[1,1] || first_char=${cwd::1}
-
-  # remove leading tilde
-  cwd="${cwd#\~}"
-
-  while [[ "$cwd" == */* && "$cwd" != "/" ]]; do
-    # pop off last part of cwd
-    local part="${cwd##*/}"
-    cwd="${cwd%/*}"
-
-    formatted_cwd="$dir_sep$part$formatted_cwd"
-    part_count=$((part_count+1))
-
-    [[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
-  done
-
-  printf "%s" "$first_char$formatted_cwd"
-}
 function __promptline_left_prompt {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
 
@@ -101,13 +65,7 @@ function __promptline_left_prompt {
   slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "b" slices
-  __promptline_wrapper "$USER" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "c" header
-  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_wrapper "$(if [[ -n ${ZSH_VERSION-} ]]; then print %n; elif [[ -n ${FISH_VERSION-} ]]; then printf "%s" "$USER"; else printf "%s" \\u; fi )" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
 
   # close sections
   printf "%s" "${reset_bg}${sep}$reset$space"
@@ -154,21 +112,18 @@ function __promptline {
   local alt_rsep=""
   local reset="${wrap}0${end_wrap}"
   local reset_bg="${wrap}49${end_wrap}"
-  local a_fg="${wrap}38;5;236${end_wrap}"
-  local a_bg="${wrap}48;5;103${end_wrap}"
-  local a_sep_fg="${wrap}38;5;103${end_wrap}"
-  local b_fg="${wrap}38;5;253${end_wrap}"
-  local b_bg="${wrap}48;5;239${end_wrap}"
-  local b_sep_fg="${wrap}38;5;239${end_wrap}"
-  local c_fg="${wrap}38;5;244${end_wrap}"
-  local c_bg="${wrap}48;5;236${end_wrap}"
-  local c_sep_fg="${wrap}38;5;236${end_wrap}"
-  local warn_fg="${wrap}38;5;215${end_wrap}"
-  local warn_bg="${wrap}48;5;239${end_wrap}"
-  local warn_sep_fg="${wrap}38;5;239${end_wrap}"
-  local y_fg="${wrap}38;5;248${end_wrap}"
-  local y_bg="${wrap}48;5;239${end_wrap}"
-  local y_sep_fg="${wrap}38;5;239${end_wrap}"
+  local a_fg="${wrap}38;5;238${end_wrap}"
+  local a_bg="${wrap}48;5;117${end_wrap}"
+  local a_sep_fg="${wrap}38;5;117${end_wrap}"
+  local b_fg="${wrap}38;5;252${end_wrap}"
+  local b_bg="${wrap}48;5;240${end_wrap}"
+  local b_sep_fg="${wrap}38;5;240${end_wrap}"
+  local warn_fg="${wrap}38;5;236${end_wrap}"
+  local warn_bg="${wrap}48;5;180${end_wrap}"
+  local warn_sep_fg="${wrap}38;5;180${end_wrap}"
+  local y_fg="${wrap}38;5;247${end_wrap}"
+  local y_bg="${wrap}48;5;240${end_wrap}"
+  local y_sep_fg="${wrap}38;5;240${end_wrap}"
   if [[ -n ${ZSH_VERSION-} ]]; then
     PROMPT="$(__promptline_left_prompt)"
     RPROMPT="$(__promptline_right_prompt)"
